9bd5807b67c29f599f3367def95a9491ce698fad
DMarrs <diegomarrs@gmail.com>
2022-11-05 15:17:39 -0700

    added form

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import Search from './lib/Search.svelte';
 </script>
 
 <main>
+  <Search></Search>
 </main>
 
 <style>
---
+++ src/lib/Search.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<div>
+    <form on:submit|preventDefault={() => console.log('hello!')}>
+        <label for="music-search">Search for music:</label>
+        <input id="music-search"/>
+        <button type="submit">Submit</button>
+    </form>
+</div>
