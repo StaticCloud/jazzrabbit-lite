acbcc5f056c8332822870983419d54eed1c16aea
DMarrs <diegomarrs@gmail.com>
2022-11-07 14:05:49 -0800

    updated search page to only search for albums

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
+    let query;
 
+    const getMusic = async () => {
+        query = query.split(" ").join("+");
+        const response = await fetch(`https://itunes.apple.com/search?country=US&term=${query}&media=music&entity=album`);
+        const data = await response.json();
+        console.log(data);
+    }
+    
 </script>
 
 <div>
-    <form on:submit|preventDefault={() => console.log('hello!')}>
+    <form on:submit|preventDefault={() => getMusic()}>
         <label for="music-search">Search for music:</label>
-        <input id="music-search"/>
+        <input id="music-search" bind:value={query}/>
         <button type="submit">Submit</button>
     </form>
 </div>
