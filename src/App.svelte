49daad5a586870c6dabdc182f0a6ef27428a1014
DMarrs <diegomarrs@gmail.com>
2022-11-07 13:39:29 -0800

    added more pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import Router from 'svelte-spa-router';
 
+  import Home from './lib/pages/Home.svelte';
   import Search from './lib/Search.svelte';
+  import NotFound from './lib/pages/NotFound.svelte';
 
   const routes = {
-    '/search': Search
+    '/': Home,
+    '/search': Search,
+    '*': NotFound,
   }
 </script>
 
