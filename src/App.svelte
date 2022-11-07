2055196e7867b0aabe8f6dd8faf5646e44743e9b
DMarrs <diegomarrs@gmail.com>
2022-11-07 13:51:20 -0800

    added more navigation links and pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
   import Router from 'svelte-spa-router';
 
+  // pages
   import Home from './lib/pages/Home.svelte';
   import Search from './lib/Search.svelte';
+  import Collection from './lib/pages/Collection.svelte';
+  import Favorites from './lib/pages/Favorites.svelte';
   import NotFound from './lib/pages/NotFound.svelte';
+
+  // components
+  import Nav from './lib/components/Nav.svelte'
 
   const routes = {
     '/': Home,
     '/search': Search,
+    '/collection': Collection,
+    '/Favorites': Favorites,
     '*': NotFound,
   }
 </script>
 
 <main>
+  <Nav/>
   <Router {routes}/>
 </main>
 
---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { link } from 'svelte-spa-router'
+</script>
+
+<nav>
+    <a href="/" use:link>Home</a>
+    <a href="/search" use:link>Search</a>
+    <a href="/collection" use:link>Collection</a>
+    <a href="/favorites" use:link>Favorites</a>
+</nav>
