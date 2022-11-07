21f684305030e1208da04d48b25866870ced71f9
DMarrs <diegomarrs@gmail.com>
2022-11-07 13:27:19 -0800

    setup svelte routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+  import Router from 'svelte-spa-router';
+
   import Search from './lib/Search.svelte';
+
+  const routes = {
+    '/search': Search
+  }
 </script>
 
 <main>
-  <Search></Search>
+  <Router {routes}/>
 </main>
 
 <style>
