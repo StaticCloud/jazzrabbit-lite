bb1b99414873614d90c6e1eaee3ae6179e022dfb
DMarrs <diegomarrs@gmail.com>
2022-11-08 11:54:26 -0800

    added grid layout to search results

--- src/lib/pages/Search.svelte
+++ src/lib/pages/Search.svelte
@@ -4,8 +4,25 @@
 </script>
 
 <SearchBar bind:searchResults="{albumData}"/>
+<div class="grid">
 {#if albumData}
     {#each albumData as album}
-        <img alt={album.collectionType} src={album.artworkUrl100.replace('100x100', '300x300')}/>
+        <div class="item"></div>
+        <!-- <img alt={album.collectionType} src={album.artworkUrl100.replace('100x100', '300x300')}/> -->
     {/each}
 {/if}
+</div>
+
+<style>
+    .grid {
+        display: grid;
+        gap: 10px;
+        grid-row: 1;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+
+    .item {
+        background-color: burlywood;
+        aspect-ratio: 1;
+    }
+</style>
