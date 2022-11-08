db40f210ed4b45368909652984cf3404883c0cca
DMarrs <diegomarrs@gmail.com>
2022-11-08 12:06:31 -0800

    added album component

---
+++ src/lib/components/Album.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let cover;
+</script>
+
+<div class="album" style="background-image: url({cover});"></div>
+
+<style>
+    .album {
+        aspect-ratio: 1;
+        background-size: cover;
+    }
+</style>
--- src/lib/pages/Search.svelte
+++ src/lib/pages/Search.svelte
@@ -1,14 +1,14 @@
 <script>
     let albumData;
     import SearchBar from '../components/SearchBar.svelte'
+    import Album from '../components/Album.svelte'
 </script>
 
 <SearchBar bind:searchResults="{albumData}"/>
 <div class="grid">
 {#if albumData}
     {#each albumData as album}
-        <div class="item"></div>
-        <!-- <img alt={album.collectionType} src={album.artworkUrl100.replace('100x100', '300x300')}/> -->
+        <Album cover={album.artworkUrl100.replace('100x100', '300x300')}></Album>
     {/each}
 {/if}
 </div>
@@ -19,10 +19,5 @@
         gap: 10px;
         grid-row: 1;
         grid-template-columns: 1fr 1fr 1fr 1fr;
-    }
-
-    .item {
-        background-color: burlywood;
-        aspect-ratio: 1;
     }
 </style>
