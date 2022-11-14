2da6946e850a5c1f76783102faccadc108ff14dd
DMarrs <diegomarrs@gmail.com>
2022-11-14 14:35:28 -0800

    added icons and changes to nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
     import { link } from 'svelte-spa-router'
+    import FaHome from 'svelte-icons/fa/FaHome.svelte'
+    import FaSearch from 'svelte-icons/fa/FaSearch.svelte'
+    import FaBook from 'svelte-icons/fa/FaBook.svelte'
+    import FaHeart from 'svelte-icons/fa/FaHeart.svelte'
 </script>
 
 <nav class="navigation">
-    <a href="/" use:link>Home</a>
-    <a href="/search" use:link>Search</a>
-    <a href="/collection" use:link>Collection</a>
-    <a href="/favorites" use:link>Favorites</a>
+    <a href="/" use:link>
+        <FaHome/>
+    </a>
+    <a href="/search" use:link>
+        <FaSearch/>
+    </a>
+    <a href="/collection" use:link>
+        <FaBook/>
+    </a>
+    <a href="/favorites" use:link>
+        <FaHeart/>
+    </a>
 </nav>
 
 <style>
+    a {
+        width: 31px;
+        height: 31px;
+
+        color: rgb(214, 141, 73);
+        padding: 25px;
+    }
+
     .navigation {
         display: flex;
         flex-direction: column;
@@ -17,7 +37,6 @@
         position: fixed;
         top: 0;
         left: 0;
-        width: 90px;
 
         height: 100vh;
         background-color: rgb(43, 43, 43);
